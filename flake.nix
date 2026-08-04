dabfb8e26c32107aad155f52b1fc4bfec9d78271
ReimersS <sebastian.reimers@tum.de>
2026-08-04 11:09:56 +0000

    Updated flake

--- flake.nix
+++ flake.nix
@@ -11,14 +11,39 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-
-      defaultPackage = pkgs.llvmPackages.stdenv.mkDerivation {
-        name = "custom-clang";
+      packages.default = pkgs.llvmPackages.stdenv.mkDerivation {
+        pname = "custom-clang";
+        version = "0.1.0";
         src = self;
-        nativeBuildInputs = pkgs.clang.nativeBuildInputs ++ [
+
+        nativeBuildInputs = [
+          pkgs.cmake
+          pkgs.ninja
+          pkgs.python3
+        ];
+
+        cmakeDir = "../llvm";
+
+        cmakeFlags = [
+          "-DLLVM_ENABLE_PROJECTS=clang"
+          "-DCMAKE_BUILD_TYPE=Release"
+          "-DLLVM_TARGETS_TO_BUILD=Native"
+          "-DLLVM_PARALLEL_LINK_JOBS=4"
+        ];
+
+        hardeningDisable = [ "all" ];
+
+        dontStrip = true;
+      };
+
+      devShells.default = pkgs.llvmPackages.stdenv.mkDerivation {
+        name = "custom-clang-devshell";
+        src = self;
+        nativeBuildInputs = [
           pkgs.cmake
           pkgs.ninja
           pkgs.gdb
+          pkgs.python3
           pkgs.llvmPackages_20.clang-tools
         ];
         hardeningDisable = [ "all" ];
